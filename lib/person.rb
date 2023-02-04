983d781610ad94b0313c9549684661c5415511fd
root <root@Ty-Laptop.localdomain>
2023-02-04 15:34:32 -0500

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
   # your code here
+  @@all = []
+
+  def initialize argument = {}
+    argument.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end 
+     @@all << self  
+  end
+
+  def self.all
+    @@all
+  end
+
+  puts "People -> #{self.all}"
+
 end
+
+brandon = Person.new ({name: "brandon", age: 29, job: "idk"})
+darian = Person.new ({first_name: "darian", age: 26, spirit_animal: "dog"})
+
+puts "People -> #{Person.all}"
